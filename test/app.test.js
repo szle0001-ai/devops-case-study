2c75f885601a3168af9d81f9f099b2d8edb3eb8e
szle0001-ai <szle0001@gmail.com>
2026-09-08 16:31:23 +0200

    Fix status endpoint test definition

--- test/app.test.js
+++ test/app.test.js
@@ -16,13 +16,15 @@ describe("Web application", () => {
     expect(response.body).toEqual({
       status: "ok"
     });
-test("GET /status returns application status", async () => {
-  const response = await request(app).get("/status");
-
-  expect(response.statusCode).toBe(200);
-  expect(response.body).toEqual({
-    application: "devops-case-study",
-    status: "running"
   });
-});
+
+  test("GET /status returns application status", async () => {
+    const response = await request(app).get("/status");
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      application: "devops-case-study",
+      status: "running"
+    });
+  });
 });
